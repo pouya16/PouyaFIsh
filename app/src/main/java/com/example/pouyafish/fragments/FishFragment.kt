5023cdf7db8da14847932c8ab61d803bee3332d1
pouya <pouya16@gmail.com>
2023-10-07 10:06:44 -0700

    structuring main view files

---
+++ app/src/main/java/com/example/pouyafish/fragments/FishFragment.kt
@@ -0,0 +1,20 @@
+package com.example.pouyafish.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pouyafish.R
+
+class FishFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fish, container, false)
+    }
+
+}
